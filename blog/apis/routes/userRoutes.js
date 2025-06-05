9724828cbfe39738ca68f4d92b8eb6733405606c
Shimanta Das <iamshimantadas@gmail.com>
2025-06-06 00:12:33 +0530

    user route module done

--- blog/apis/routes/userRoutes.js
+++ blog/apis/routes/userRoutes.js
@@ -12,17 +12,7 @@ const UserSchema = new mongoose.Schema({
     'phone': String
 });
 
-router.get("/user", async (req, res) => {
-    try {
-        const db = await Mongo();
-        const User = db.model('users', UserSchema);
-        const result = await User.find({}).exec();
-        res.status(200).send(result);
-    } catch (err) {
-        res.status(400).send({ "status": "failed" });
-    }
-});
-
+// user's registration
 router.post("/user/register", async (req, res) => {
     try {
         const db = await Mongo();
@@ -42,6 +32,7 @@ router.post("/user/register", async (req, res) => {
     }
 });
 
+// user's login
 router.post("/user/login", async (req, res) => {
     try {
         const db = await Mongo();
@@ -58,6 +49,53 @@ router.post("/user/login", async (req, res) => {
         }
     } catch (err) {
         res.status(401).send({ "status": "failed" });
+    }
+});
+
+// all users
+router.get("/user", async (req, res) => {
+    try {
+        const db = await Mongo();
+        const User = db.model('users', UserSchema);
+        const result = await User.find({}).exec();
+        res.status(200).send(result);
+    } catch (err) {
+        res.status(400).send({ "status": "failed" });
+    }
+});
+
+// fund by user id
+router.get("/user/:id", async (req, res) => {
+    try {
+        const db = await Mongo();
+        const User = db.model('users', UserSchema);
+        const result = await User.findById(req.params.id).exec();
+        res.status(200).send(result);
+    } catch (err) {
+        res.status(400).send({ "status": "failed" });
+    }
+});
+
+// update user
+router.put("/user", async (req, res) => {
+    try {
+        const db = await Mongo();
+        const User = db.model('users', UserSchema);
+        const fields = await User.findOne({"_id":req.body.id});
+        if(req.body.name){
+            fields.name = req.body.name;
+        }
+        if(req.body.email){
+            fields.email = req.body.email;
+        }
+        if(req.body.password){
+            fields.password = req.body.password;
+        }
+        fields.save();
+        
+        res.status(201).send({"status":"success", "message":"user record updated"});
+    } catch (err) {
+        res.status(400).send({ "status": "failed" });
     }
 });
 
